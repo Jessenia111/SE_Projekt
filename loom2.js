7215c2bdf48da753917ed719dfa803266a490f85
tincanned <chukhonkinatanya@gmail.com>
2024-11-29 10:32:22 +0200

    lehe 1. osa. lisan sisu html ja css failile. loon js fail, et oleks kergem lugeda. hiljem tegelen 2. osaga. vajadusel muudan esimest.

---
+++ loom2.js
@@ -0,0 +1,57 @@
+function toggleDetails(id) {
+    const element = document.getElementById(id);
+    element.classList.toggle('visible');
+}
+let currentImageIndex = 0; // alustab 1 pildiga
+const images = document.querySelectorAll('.parallax'); // parallax tagis olevad pildid
+
+function showImage(index) {
+  // eemaldas 'active' piltidest
+  images.forEach((img, i) => {
+    img.classList.remove('active');
+    if (i === index) {
+      img.classList.add('active'); // lisab 'active' valitud pildile
+    }
+  });
+}
+
+function prevImage() {
+  currentImageIndex = (currentImageIndex - 1 + images.length) % images.length; // tagasi viimasele pildile
+  showImage(currentImageIndex);
+}
+
+function nextImage() {
+  currentImageIndex = (currentImageIndex + 1) % images.length; // tagasi 1. pildile
+  showImage(currentImageIndex);
+}
+
+// näitab 1 praegune pilt
+showImage(currentImageIndex);
+
+
+const modal = document.getElementById('imageModal');
+const modalImage = document.getElementById('modalImage');
+
+// funktsioon et avada modal ja näitama pilt
+function enlargeImage(src) {
+    modal.style.display = 'block';
+    modalImage.src = src; // img allik on vajutatud img
+}
+
+function closeModal() {
+    modal.style.display = 'none';
+}
+
+// kehtib kõikide image-details piltide jaoks
+document.querySelectorAll('.details-img').forEach(img => {
+    img.addEventListener('click', () => enlargeImage(img.src));
+});
+// mõelda organisatsiooni npeale
+document.querySelectorAll('.accordion-btn').forEach(button => {
+    button.addEventListener('click', () => {
+        const content = button.nextElementSibling;
+
+       
+        content.style.display = content.style.display === 'block' ? 'none' : 'block';
+    });
+});
